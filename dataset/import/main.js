916c2cc6f75a7b5073860363aa789cf936827960
cschnabl <mail@christoph-schnabl.com>
2018-11-17 15:33:57 +0100

    Add map

--- dataset/import/main.js
+++ dataset/import/main.js
@@ -1,9 +1,17 @@
 const fs = require('fs');
 
+let assign = function(element) {
+    return element + "hansi";
+}
+
 let fileStream = fs.createReadStream("../german-credit.data");
 
 fileStream.on("data", function(data) {
     let chunk = data.toString();
 
-    console.log(chunk);
+    let lines = chunk.split('\n'); //split the chunks into lines
+
+    lines.forEach(function(line) {
+        line.split(" ").map(assign)); //split the lines into columns due to their seperation by a space
+    });
 });
