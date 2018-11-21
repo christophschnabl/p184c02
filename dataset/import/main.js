e9085fb54a859aaa0656c6e1824f67904bd28684
cschnabl <mail@christoph-schnabl.com>
2018-11-21 10:56:19 +0100

    Change double to single quotes

--- dataset/import/main.js
+++ dataset/import/main.js
@@ -1,17 +1,17 @@
 const fs = require('fs');
 
 let assign = function(element) {
-    return element + "hansi";
+    return element + hansi';
 }
 
-let fileStream = fs.createReadStream("../german-credit.data");
+const fileStream = fs.createReadStream('../german-credit.data');
 
-fileStream.on("data", function(data) {
-    let chunk = data.toString();
+fileStream.on('data', function(data) {
+    const chunk = data.toString();
 
-    let lines = chunk.split('\n'); //split the chunks into lines
+    const lines = chunk.split('\n'); //split the chunks into lines
 
     lines.forEach(function(line) {
-        line.split(" ").map(assign)); //split the lines into columns due to their seperation by a space
+        line.split(' ').map(assign)); //split the lines into columns due to their seperation by a space
     });
 });
