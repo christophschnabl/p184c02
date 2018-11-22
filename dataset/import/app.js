76233387e0108dff16972c4a5cd7928170928168
Andreas Horvath <mail@andreas-horvath.at>
2018-11-22 11:37:08 +0100

    Add Database query

--- dataset/import/app.js
+++ dataset/import/app.js
@@ -7,6 +7,12 @@ async function importFromFile() {
     const customerData = await readGermanCreditData('../german-credit.data');
     const creditCardData = await readCreditCards(['../credit-cards/credit-cards-1:5.json']);
     console.log(customerData, creditCardData);
+
+    const res = await pool.query('select 1+1');
+
+    console.log(res);
+
+    await pool.end();
 }
 
 
