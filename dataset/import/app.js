ae0783f2687eb24c662d45bce17a28c3bc35264c
Andreas Horvath <mail@andreas-horvath.at>
2018-11-27 13:21:23 +0100

    Add list of credit-cards json files

--- dataset/import/app.js
+++ dataset/import/app.js
@@ -5,7 +5,13 @@ const pool = require('./modules/database.js');
 
 async function importFromFile() {
     const customerData = await readGermanCreditData('../german-credit.data');
-    const creditCardData = await readCreditCards(['../credit-cards/credit-cards-1:5.json']);
+    const creditCardData = await readCreditCards([
+        '../credit-cards/credit-cards-1:5.json',
+        '../credit-cards/credit-cards-2:5.json',
+        '../credit-cards/credit-cards-3:5.json',
+        '../credit-cards/credit-cards-4:5.json',
+        '../credit-cards/credit-cards-5:5.json'
+    ]);
 
     await pool.query(queries.customerInsert, customerData);
     await pool.query(queries.creditCardInsert, creditCardData);
