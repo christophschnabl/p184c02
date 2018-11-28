55224fe9f01f0cb1930e35f6ee552a44b97be0d7
pkain <peter.kain@gmx.at>
2018-11-28 18:00:17 +0100

    Fix table name and add column names for insert, delete unused mock-ups

--- dataset/import/modules/queries.js
+++ dataset/import/modules/queries.js
@@ -1,4 +1,7 @@
 module.exports = {
-    customerInsert: `insert into customers () values ?`,
+    customerInsert: `insert into customer (CustomerUUID, Address, AccountStatus, Duration, CreditHistory, Purpose, CreditAmount, Savings, 
+                                           Employment, InstallmentRate, PersonalStatus, sex, Debtors, Residence, Property, Age,
+                                           OtherInstallmentPlans, Housing, NumberOfCredits, Job, LiableMaintenance, Telephone, ForeignWorker, Cost) 
+                     values ?`,
     creditCardInsert: `insert into creditcard () values ?`
 };
