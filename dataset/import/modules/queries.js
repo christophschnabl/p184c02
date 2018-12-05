b976b54d92a5ca3e4392552223769260317455b6
Andreas Horvath <mail@andreas-horvath.at>
2018-12-05 21:05:49 +0100

    Add CreditCard insert statement and add attributes to customer insert

--- dataset/import/modules/queries.js
+++ dataset/import/modules/queries.js
@@ -1,7 +1,7 @@
 module.exports = {
-    customerInsert: `insert into Customer (AccountStatus, Duration, CreditHistory, Purpose, CreditAmount,
+    customerInsert: `insert into Customer (CustomerUUID, Name, Country, Address, AccountStatus, Duration, CreditHistory, Purpose, CreditAmount,
         Savings, Employment, InstallmentRate, PersonalStatus, sex, Debtors, Residence, Property, Age,
         OtherInstallmentPlans, Housing, NumberOfCredits, Job, LiableMaintenance, Telephone, ForeignWorker, Cost)
         values ?`,
-    creditCardInsert: `insert into CreditCard () values ?`
+    creditCardInsert: `insert into CreditCard (CardNumber, IssuingNetwork, CVV, ExpirationYear, ExpirationMonth, CustomerUUID) values ?`
 };
