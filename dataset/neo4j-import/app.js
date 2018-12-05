73819ba4c07680a1beaa27118ed292eb8378fd34
Andreas Horvath <mail@andreas-horvath.at>
2018-12-05 21:16:53 +0100

    Add neo4j database access module

--- dataset/neo4j-import/app.js
+++ dataset/neo4j-import/app.js
@@ -1,13 +1,19 @@
 const pool = require('./modules/database.js');
+const driver = require('./modules/neo4j.js');
 const queries = require('./modules/queries.js');
 
 async function importFromMysql() {
+    const session = driver.session();
+
     try {
 
     } catch (e) {
         console.warn('An error occured', e);
     }
 
+
+    session.close();
+    driver.close();
 
     await pool.end();
 }
