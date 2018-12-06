78960b7775692bd2d34a242409b1c3445e5fbbf9
Andreas Horvath <mail@andreas-horvath.at>
2018-12-06 09:29:25 +0100

    Write neo4j import

--- dataset/neo4j-import/app.js
+++ dataset/neo4j-import/app.js
@@ -6,6 +6,20 @@ async function importFromMysql() {
     const session = driver.session();
 
     try {
+        const [customers, _cufields] =
+            await pool.query(queries.customerSelect);
+        const [creditcards, _crfields] =
+            await pool.query(queries.creditCardSelect);
+
+        for (const customer of customers) {
+            await session.run(`CREATE (customer:Customer {
+                id: {CustomerUUID},
+                name: {Name},
+                country: {Country},
+                address: {Address},
+                accountStatus: {AccountStatus}
+            })`, customer);
+        }
 
     } catch (e) {
         console.warn('An error occured', e);
--- dataset/neo4j-import/modules/queries.js
+++ dataset/neo4j-import/modules/queries.js
@@ -1,3 +1,6 @@
 module.exports = {
-
+    customerSelect: `select CustomerUUID, Name, Country, Address, AccountStatus, Duration, CreditHistory, Purpose, CreditAmount,
+        Savings, Employment, InstallmentRate, PersonalStatus, sex, Debtors, Residence, Property, Age,
+        OtherInstallmentPlans, Housing, NumberOfCredits, Job, LiableMaintenance, Telephone, ForeignWorker, Cost from Customer`,
+    creditCardSelect: `select CardNumber, IssuingNetwork, CVV, ExpirationYear, ExpirationMonth, CustomerUUID from CreditCard`
 };
