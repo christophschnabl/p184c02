80cd18e49e0013e540c88eff58d8fd22e95c9abc
pkain <peter.kain@gmx.at>
2018-12-12 20:21:30 +0100

    Add usability of telephoneNumbers.js

--- dataset/telephoneNumbers.js
+++ dataset/telephoneNumbers.js
@@ -2,3 +2,14 @@
 function createTelephoneNumber() {
     return Math.floor(Math.random() * 899999999999) + 100000000000;
 }
+
+/** Creates n telephone numbers and returns them in a string separated by a new line */
+function createTelephoneNumbers(num) {
+    let numbers = '';
+    for(let i = 0; i < num; i += 1) {
+        numbers += createTelephoneNumber.toString() + '\n';
+    }
+    return numbers;
+}
+
+module.exports = createTelephoneNumbers;
