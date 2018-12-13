048ba6d635fca4c2e4512183b7429d05114ab0f8
Andreas Horvath <mail@andreas-horvath.at>
2018-12-13 10:06:37 +0100

    Add script to add fraud rings

--- dataset/addFraud/app.js
+++ dataset/addFraud/app.js
@@ -4,7 +4,13 @@ const queries = require('./modules/queries.js');
 
 async function addFraud() {
     try {
+        console.log('Adding fraud rings...');
 
+        for (const query of queries) {
+            await pool.query(query);
+        }
+
+        console.log(`Did ${queries.length} SQL updates.`);
     } catch (e) {
         console.warn('An error occured', e);
     }
--- dataset/addFraud/modules/queries.js
+++ dataset/addFraud/modules/queries.js
@@ -1,3 +1,17 @@
-module.exports = {
-
-};
+module.exports = [
+    // fix bug here
+    `update Customer
+        set SSN = (select CustomerUUID
+                     from (select CustomerUUID
+                             from Customer) c
+                    where CustomerUUID = 1)
+      where CustomerUUID = 2;
+    `,
+    `update Customer
+        set SSN = (select CustomerUUID
+                     from (select CustomerUUID
+                             from Customer) c
+                    where CustomerUUID = 1)
+      where CustomerUUID = 3;
+    `
+];
