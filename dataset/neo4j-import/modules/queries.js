cccbb4d748dda2473d25c209d2b166abd9b2481c
Andreas Horvath <mail@andreas-horvath.at>
2018-12-13 09:08:05 +0100

    Fix neo4j import script error

--- dataset/neo4j-import/modules/queries.js
+++ dataset/neo4j-import/modules/queries.js
@@ -1,5 +1,5 @@
 module.exports = {
-    customerSelect: `select CustomerUUID, Name, Country, Address, AccountStatus, Duration, CreditHistory, Purpose, CreditAmount,
+    customerSelect: `select CustomerUUID, Name, Country, Address, SSN, AccountStatus, Duration, CreditHistory, Purpose, CreditAmount,
         Savings, Employment, InstallmentRate, PersonalStatus, sex, Debtors, Residence, Property, Age,
         OtherInstallmentPlans, Housing, NumberOfCredits, Job, LiableMaintenance, Telephone, ForeignWorker, Cost from Customer`,
     creditCardSelect: `select CardNumber, IssuingNetwork, CVV, ExpirationYear, ExpirationMonth, CustomerUUID from CreditCard`
