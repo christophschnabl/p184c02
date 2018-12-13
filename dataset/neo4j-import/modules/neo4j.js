02a769a9fb6e00eae66658a310f85626ba50b4b6
Andreas Horvath <mail@andreas-horvath.at>
2018-12-13 10:07:07 +0100

    Change environment variable names

--- dataset/neo4j-import/modules/neo4j.js
+++ dataset/neo4j-import/modules/neo4j.js
@@ -1,8 +1,8 @@
 const neo4j = require('neo4j-driver').v1;
 
-const user = process.env.NEO4j_USER || 'neo4j';
-const password = process.env.NEO4j_PASSWORD || 'neo4j';
-const uri = process.env.NEO4j_URI || 'bolt://localhost:7687';
+const user = process.env.NEO4J_USER || 'neo4j';
+const password = process.env.NEO4J_PASSWORD || 'neo4j';
+const uri = process.env.NEO4J_URI || 'bolt://localhost:7687';
 
 const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
 
