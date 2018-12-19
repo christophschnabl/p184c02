4096b1a318bb93072c343f405bc2532b0ccbe6b8
pkain <peter.kain@gmx.at>
2018-12-19 19:40:36 +0100

    Fix typo

--- dataset/fraudQueries/genQueries.py
+++ dataset/fraudQueries/genQueries.py
@@ -1,8 +1,8 @@
-nodes = [":SSN", ":Address", ":Phone", ":Creditcard"]
+nodes = [":SSN", ":Address", ":Phone", ":CreditCard"]
 relations = [":HAS_SSN", ":HAS_ADDRESS", ":USES_PHONENUMBER", ":USES_CREDITCARD"]
 filename = str(input("Filename: "))
 
-print("0 = SSN, 1 = Address, 2 = Phonenumber, 3 = Creditcard")
+print("0 = SSN, 1 = Address, 2 = Phonenumber, 3 = CreditCard")
 node = int(input("Node: "));
 
 file = open(filename, "w");
