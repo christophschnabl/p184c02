d0ca60eb5aaf626e69ec4dc2fe2e4fd59056a067
Andreas Horvath <mail@andreas-horvath.at>
2018-12-19 23:34:45 +0100

    Change let to const

--- dataset/create-table/app.js
+++ dataset/create-table/app.js
@@ -2,8 +2,8 @@ const pool = require('./modules/database.js');
 const fs = require('fs');
 
 async function createTable() {
-    let sql =  fs.readFile('./create-table.sql');
-    
+    const sql = fs.readFile('./create-table.sql');
+
     try {
         await pool.query(sql);
     } catch (e) {
