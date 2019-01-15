2d52eecf00e83cb650cb3a6ee435bf5ea4343fd7
Andreas Horvath <mail@andreas-horvath.at>
2019-01-15 14:07:51 +0100

    Add wait function

--- dataset/replication/app.js
+++ dataset/replication/app.js
@@ -1,1 +1,16 @@
 const pool = require('./modules/database.js');
+
+function wait(ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, ms);
+    })
+}
+
+async function replicationLoop() {
+    while (true) {
+
+        await wait(10000);
+    }
+}
+
+replicationLoop();
