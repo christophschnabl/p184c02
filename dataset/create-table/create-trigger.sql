cede7b1a9c96f1402d6e76c0d31924d580fab52a
Fromwald <fabianfromwald@gmx.at>
2019-01-15 16:46:49 +0100

    Update create-trigger.sql

--- dataset/create-table/create-trigger.sql
+++ dataset/create-table/create-trigger.sql
@@ -1,10 +1,12 @@
 create trigger AfterInsCustomer
     after insert on Customer 
     for each row
-    insert into Customer_Polling values (new.CustomerUUID);
+    insert into Customer_Polling values (new.CustomerUUID)
+;
 
 
 create trigger AfterUpdCustomer
     after update on Customer 
     for each row
-    insert into Customer_Polling (CustomerUUID) values (new.CustomerUUID);
+    insert into Customer_Polling (CustomerUUID) values (new.CustomerUUID)
+;
