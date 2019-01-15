73db1c1fe4f2fd912b8916344f04ac635929cad7
Andreas Horvath <mail@andreas-horvath.at>
2019-01-15 14:02:25 +0100

    Add replication package

---
+++ dataset/replication/app.js
@@ -0,0 +1,1 @@
+const pool = require('./modules/database.js');
---
+++ dataset/replication/modules/database.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool(
+    {
+        host: process.env.MYSQL_HOST || 'localhost',
+        user: process.env.MYSQL_USER || 'frauddetection',
+        password: process.env.MYSQL_PASSWORD || 'frauddetection',
+        database: process.env.MYSQL_DB || 'frauddetection'
+    }
+);
+
+module.exports = pool.promise();
