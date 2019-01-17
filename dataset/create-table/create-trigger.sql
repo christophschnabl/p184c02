46ce3a7a0760db82c7fb8390f2cf604ef3d865ae
Fromwald <fabianfromwald@gmx.at>
2019-01-17 08:51:38 +0100

    Add comments to create-trigger.sql

--- dataset/create-table/create-trigger.sql
+++ dataset/create-table/create-trigger.sql
@@ -1,15 +1,19 @@
+-- Customer Triggers
+
 create trigger AfterInsCustomer
     after insert on Customer 
     for each row
     insert into Customer_Polling values (new.CustomerUUID)
 ;
 
-
 create trigger AfterUpdCustomer
     after update on Customer 
     for each row
     insert into Customer_Polling (CustomerUUID) values (new.CustomerUUID)
 ;
+
+
+--CreditCard Triggers
 
 create trigger AfterInsCreditCard
 after insert on CreditCard 
