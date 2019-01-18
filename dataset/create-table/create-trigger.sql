bd847b69c8877e029b7ed39baf7e5ea9de04a24a
Fromwald <fabianfromwald@gmx.at>
2019-01-18 09:28:45 +0100

    Adapt triggers to new polling table design

--- dataset/create-table/create-trigger.sql
+++ dataset/create-table/create-trigger.sql
@@ -3,13 +3,25 @@
 create trigger AfterInsCustomer
     after insert on Customer 
     for each row
-    insert into Customer_Polling values (new.CustomerUUID)
+    insert into Customer_Polling values (new.CustomerUUID, new.Name, new.Country, new.Address, new.SSN,
+                                         new.Telephone, new.AccountStatus, new.Duration, new.CreditHistory,
+                                         new.Purpose, new.CreditAmount, new.Savings, new.Employment, 
+                                         new.InstallmentRate, new.PersonalStatus, new.sex, new.Debtors,
+                                         new.Residence, new.Property, new.Age, new.OtherInstallmentPlans,
+                                         new.Housing, new.NumberOfCredits, new.Job, new.LiableMaintenance,
+                                         new.ForeignWorker, new.Cost, false)
 ;
 
 create trigger AfterUpdCustomer
     after update on Customer 
     for each row
-    insert into Customer_Polling (CustomerUUID) values (new.CustomerUUID)
+    insert into Customer_Polling values (new.CustomerUUID, new.Name, new.Country, new.Address, new.SSN,
+                                         new.Telephone, new.AccountStatus, new.Duration, new.CreditHistory,
+                                         new.Purpose, new.CreditAmount, new.Savings, new.Employment, 
+                                         new.InstallmentRate, new.PersonalStatus, new.sex, new.Debtors,
+                                         new.Residence, new.Property, new.Age, new.OtherInstallmentPlans,
+                                         new.Housing, new.NumberOfCredits, new.Job, new.LiableMaintenance,
+                                         new.ForeignWorker, new.Cost, false)
 ;
 
 
@@ -18,11 +30,13 @@ create trigger AfterUpdCustomer
 create trigger AfterInsCreditCard
 after insert on CreditCard 
     for each row
-    insert into CreditCard_Polling (CardNumber) values (new.CardNumber)
+    insert into CreditCard_Polling (CardNumber) values (new.CardNumber, new.IssuingNetwork, new.CVV,
+                                                        new.ExpirationMonth, new.ExpirationYear, false)
 ;
 
 create trigger AfterUpdCreditCard
 after update on CreditCard 
     for each row
-    insert into CreditCard_Polling (CardNumber) values (new.CardNumber)
+    insert into CreditCard_Polling (CardNumber) values (new.CardNumber, new.IssuingNetwork, new.CVV,
+                                                        new.ExpirationMonth, new.ExpirationYear, false)
 ;
