12294d0cd17d8686a88c4ff8aa3ddef94dafb49a
Fromwald <fabianfromwald@gmx.at>
2019-01-18 09:32:57 +0100

    Add delete trigger for Customer and CreditCard

--- dataset/create-table/create-trigger.sql
+++ dataset/create-table/create-trigger.sql
@@ -24,6 +24,18 @@ create trigger AfterUpdCustomer
                                          new.ForeignWorker, new.Cost, false)
 ;
 
+create trigger AfterDelCustomer
+    after delete on Customer 
+    for each row
+    insert into Customer_Polling values (new.CustomerUUID, new.Name, new.Country, new.Address, new.SSN,
+                                         new.Telephone, new.AccountStatus, new.Duration, new.CreditHistory,
+                                         new.Purpose, new.CreditAmount, new.Savings, new.Employment, 
+                                         new.InstallmentRate, new.PersonalStatus, new.sex, new.Debtors,
+                                         new.Residence, new.Property, new.Age, new.OtherInstallmentPlans,
+                                         new.Housing, new.NumberOfCredits, new.Job, new.LiableMaintenance,
+                                         new.ForeignWorker, new.Cost, true)
+;
+
 
 --CreditCard Triggers
 
@@ -39,4 +51,11 @@ after update on CreditCard
     for each row
     insert into CreditCard_Polling (CardNumber) values (new.CardNumber, new.IssuingNetwork, new.CVV,
                                                         new.ExpirationMonth, new.ExpirationYear, false)
+;
+
+create trigger AfterDelCreditCard
+after delete on CreditCard 
+    for each row
+    insert into CreditCard_Polling (CardNumber) values (new.CardNumber, new.IssuingNetwork, new.CVV,
+                                                        new.ExpirationMonth, new.ExpirationYear, true)
 ;
