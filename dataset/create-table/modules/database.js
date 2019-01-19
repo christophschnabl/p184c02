8d645f9ab1c9f6e9bd7a8b89fdd2e54b240c9b2c
Fromwald <fabianfromwald@gmx.at>
2019-01-19 15:13:38 +0100

    Enable multibleStatements for create-table script

--- dataset/create-table/modules/database.js
+++ dataset/create-table/modules/database.js
@@ -5,7 +5,8 @@ const pool = mysql.createPool(
         host: process.env.MYSQL_HOST || 'localhost',
         user: process.env.MYSQL_USER || 'frauddetection',
         password: process.env.MYSQL_PASSWORD || 'frauddetection',
-        database: process.env.MYSQL_DB || 'frauddetection'
+        database: process.env.MYSQL_DB || 'frauddetection',
+        multipleStatements: true
     }
 );
 
