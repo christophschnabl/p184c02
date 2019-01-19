7f90048116a240d0a546fc3476eeef410cc3c10c
Fromwald <fabianfromwald@gmx.at>
2019-01-19 16:03:34 +0100

    Add create-trigger to CreateTable script

--- dataset/create-table/app.js
+++ dataset/create-table/app.js
@@ -5,10 +5,12 @@ const util = require('util');
 const readFile = util.promisify(fs.readFile);
 
 async function createTable() {
-    const sql = (await readFile('./create-table.sql')).toString();
+    const sqlTable = (await readFile('./create-table.sql')).toString();
+    const sqlTrigger = (await readFile('./create-trigger.sql')).toString();
 
     try {
-        await pool.query(sql);
+        await pool.query(sqlTable);
+        await pool.query(sqlTrigger);
     } catch (e) {
         console.warn('An error occured', e);
     } finally {
