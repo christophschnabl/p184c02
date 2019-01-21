91f22312bad7b25f03395de4413671d2ed52118f
Fromwald <fabianfromwald@gmx.at>
2019-01-21 13:39:25 +0100

    Adapt triggers to new polling table design

--- dataset/create-table/create-trigger.sql
+++ dataset/create-table/create-trigger.sql
@@ -15,7 +15,7 @@ begin
                                          new.InstallmentRate, new.PersonalStatus, new.sex, new.Debtors,
                                          new.Residence, new.Property, new.Age, new.OtherInstallmentPlans,
                                          new.Housing, new.NumberOfCredits, new.Job, new.LiableMaintenance,
-                                         new.ForeignWorker, new.Cost, false);
+                                         new.ForeignWorker, new.Cost, 'ins');
 end;
 
 create trigger AfterUpdCustomer
@@ -28,7 +28,7 @@ begin
                                          new.InstallmentRate, new.PersonalStatus, new.sex, new.Debtors,
                                          new.Residence, new.Property, new.Age, new.OtherInstallmentPlans,
                                          new.Housing, new.NumberOfCredits, new.Job, new.LiableMaintenance,
-                                         new.ForeignWorker, new.Cost, false);
+                                         new.ForeignWorker, new.Cost, 'upd');
 end;
 
 create trigger AfterDelCustomer
@@ -41,7 +41,7 @@ begin
                                         old.InstallmentRate, old.PersonalStatus, old.sex, old.Debtors,
                                         old.Residence, old.Property, old.Age, old.OtherInstallmentPlans,
                                         old.Housing, old.NumberOfCredits, old.Job, old.LiableMaintenance,
-                                        old.ForeignWorker, old.Cost, true);                                   
+                                        old.ForeignWorker, old.Cost, 'del');                                   
 end;
 
 
@@ -58,7 +58,7 @@ after insert on CreditCard
 for each row
 begin
     insert into CreditCard_Polling values (new.CardNumber, new.IssuingNetwork, new.CVV,
-                                           new.ExpirationMonth, new.ExpirationYear, false);
+                                           new.ExpirationMonth, new.ExpirationYear, 'ins');
 end;
 
 create trigger AfterUpdCreditCard
@@ -66,7 +66,7 @@ after update on CreditCard
     for each row
 begin
     insert into CreditCard_Polling values (new.CardNumber, new.IssuingNetwork, new.CVV,
-                                           new.ExpirationMonth, new.ExpirationYear, false);
+                                           new.ExpirationMonth, new.ExpirationYear, 'upd');
 end;
 
 create trigger AfterDelCreditCard
@@ -74,7 +74,7 @@ after delete on CreditCard
     for each row
 begin
     insert into CreditCard_Polling values (old.CardNumber, old.IssuingNetwork, old.CVV,
-                                           old.ExpirationMonth, old.ExpirationYear, true);
+                                           old.ExpirationMonth, old.ExpirationYear, 'del');
 end;
 
 
@@ -90,21 +90,21 @@ create trigger AfterInsCustomerCreditCard
 after insert on CustomerCreditCard
 for each row
 begin
-    insert into CustomerCreditCard_Polling values (new.CustomerUUID, new.CardNumber, false);
+    insert into CustomerCreditCard_Polling values (new.CustomerUUID, new.CardNumber, 'ins');
 end;
 
 create trigger AfterUpdCustomerCreditCard
 after update on CustomerCreditCard
 for each row
 begin
-    insert into CustomerCreditCard_Polling values (new.CustomerUUID, new.CardNumber, false);
+    insert into CustomerCreditCard_Polling values (new.CustomerUUID, new.CardNumber, 'upd');
 end;
 
 create trigger AfterDelCustomerCreditCard
 after delete on CustomerCreditCard
 for each row
 begin
-    insert into CustomerCreditCard_Polling values (old.CustomerUUID, old.CardNumber, true);
+    insert into CustomerCreditCard_Polling values (old.CustomerUUID, old.CardNumber, 'del');
 end;
 
 
@@ -120,19 +120,19 @@ create trigger AfterInsTransaction
 after insert on Transaction
 for each row
 begin
-    insert into Transaction_Polling values (new.TransactionID, new.Date, new.Amount, new.CardNumber, false);
+    insert into Transaction_Polling values (new.TransactionID, new.Date, new.Amount, new.CardNumber, 'ins');
 end;
 
 create trigger AfterUpdTransaction
 after update on Transaction
 for each row
 begin
-    insert into Transaction_Polling values (new.TransactionID, new.Date, new.Amount, new.CardNumber, false);
+    insert into Transaction_Polling values (new.TransactionID, new.Date, new.Amount, new.CardNumber, 'upd');
 end;
 
 create trigger AfterDelTransaction
 after delete on Transaction
 for each row
 begin
-    insert into Transaction_Polling values (old.TransactionID, old.Date, old.Amount, old.CardNumber, true);
+    insert into Transaction_Polling values (old.TransactionID, old.Date, old.Amount, old.CardNumber, 'del');
 end;
