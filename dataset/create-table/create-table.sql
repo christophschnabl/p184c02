f26d97528a3ff669274bec3f2ea2f80ac2a5499e
Fromwald <fabianfromwald@gmx.at>
2019-01-22 07:42:11 +0100

    Change enums to varchar in polling-tables

--- dataset/create-table/create-table.sql
+++ dataset/create-table/create-table.sql
@@ -77,42 +77,33 @@ create table Transaction (
 /*Polling Tables*/
 
 create table Customer_Polling (
-    CustomerUUID            integer          not null
-,   Name                    varchar(255)     not null
-,   Country                 varchar(255)     not null
-,   Address                 varchar(255)     not null
-,   SSN                     varchar(255)     not null
-,   Telephone               varchar(32)      not null
-,   AccountStatus           enum('<0', '0-200', '>200', 'no checking account')    not null
-,   Duration                integer          not null
-,   CreditHistory           enum('no credits/all paid back',
-                                 'credits paid back at this bank ',
-                                 'credits paid back till now',
-                                 'delay in paying',
-                                 'critical account')    not null
-,   Purpose                 enum('car (new)', 'car (used)', 'furniture/equipment',
-                                 'radio/television', 'domestic appliances', 'repairs',
-                                 'education', 'vacation', 'retraining', 'business', 'others')    not null
-,   CreditAmount            integer          not null
-,   Savings                 enum('<100', '<500', '<1000', '>1000', 'unknown')
-,   Employment              enum('unemployed', '<1', '1-4', '4-7', '>7')    not null
-,   InstallmentRate         integer          not null
-,   PersonalStatus          enum('single', 'divorced/separated', 'married/widowed', 'divorced/separated/married')    not null
-,   sex                     enum('male', 'female')    not null
-,   Debtors                 enum('none', 'co-applicant', 'guarantor')    not null
+    CustomerUUID            integer             not null
+,   Name                    varchar(255)        not null
+,   Country                 varchar(255)        not null
+,   Address                 varchar(255)        not null
+,   SSN                     varchar(255)        not null
+,   Telephone               varchar(32)         not null
+,   AccountStatus           varchar(20)         not null
+,   Duration                integer             not null
+,   CreditHistory           varchar(35)         not null
+,   Purpose                 varchar(20)         not null
+,   CreditAmount            integer             not null
+,   Savings                 varchar(10)
+,   Employment              varchar(10)         not null
+,   InstallmentRate         integer             not null
+,   PersonalStatus          varchar(30)         not null
+,   sex                     varchar(10)         not null
+,   Debtors                 varchar(15)         not null
 ,   Residence               integer
-,   Property                enum('real estate', 'building society savings agreement/life insurance','car or other', 'unknown / no property')    not null
-,   Age                     integer          not null
-,   OtherInstallmentPlans   enum('bank', 'stores', 'none')    not null
-,   Housing                 enum('rent', 'own', 'for free')    not null
-,   NumberOfCredits         integer          not null
-,   Job                     enum('unskilled - none-resident',
-                                'unskilled - resident',
-                                'skilled employee',
-                                'highly qualified employee')    not null
-,   LiableMaintenance       integer          not null
-,   ForeignWorker           boolean          not null
-,   Cost                    enum('good','bad')    not null
+,   Property                varchar(50)         not null
+,   Age                     integer             not null
+,   OtherInstallmentPlans   varchar(10)         not null
+,   Housing                 varchar(10)         not null
+,   NumberOfCredits         integer             not null
+,   Job                     varchar(25)         not null
+,   LiableMaintenance       integer             not null
+,   ForeignWorker           boolean             not null
+,   Cost                    varchar(5)          not null
 ,   Action                  enum('ins', 'upd', 'del')   not null
 ,   primary key (CustomerUUID)
 );
