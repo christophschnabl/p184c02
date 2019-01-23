76e1f391fb24ede3d6001f0dbe4190686958aa37
cschnabl <mail@christoph-schnabl.com>
2019-01-23 19:32:25 +0100

    Add polling queries to polling module

--- dataset/replication/modules/queries.js
+++ dataset/replication/modules/queries.js
@@ -1,2 +1,8 @@
 module.exports = {
+    customerPollingSelect: `select CustomerUUID, Name, Country, Address, SSN, AccountStatus, Duration, CreditHistory, Purpose, CreditAmount,
+        Savings, Employment, InstallmentRate, PersonalStatus, sex, Debtors, Residence, Property, Age,
+        OtherInstallmentPlans, Housing, NumberOfCredits, Job, LiableMaintenance, Telephone, ForeignWorker, Cost from Customer_Polling`,
+    creditCardPollingSelect: `select CardNumber, IssuingNetwork, CVV, ExpirationYear, ExpirationMonth from CreditCard_Polling`,
+    customerCreditCardPollingSelect: `select CustomerUUID, CardNumber from CustomerCreditCard_Polling`,
+    myCreditCardPollingSelect: `select CardNumber, IssuingNetwork, CVV, ExpirationYear, ExpirationMonth from CreditCard_Polling where CustomerUUID = ?`
 };
