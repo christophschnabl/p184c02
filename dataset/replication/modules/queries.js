7a4849b1a3136869b64142de8884bb9b4bbccef4
cschnabl <mail@christoph-schnabl.com>
2019-01-23 22:01:23 +0100

    Add delete queries for polling

--- dataset/replication/modules/queries.js
+++ dataset/replication/modules/queries.js
@@ -3,5 +3,8 @@ module.exports = {
         Savings, Employment, InstallmentRate, PersonalStatus, sex, Debtors, Residence, Property, Age,
         OtherInstallmentPlans, Housing, NumberOfCredits, Job, LiableMaintenance, Telephone, ForeignWorker, Cost from Customer_Polling`,
     creditCardPollingSelect: `select CardNumber, IssuingNetwork, CVV, ExpirationYear, ExpirationMonth from CreditCard_Polling`,
-    customerCreditCardPollingSelect: `select CustomerUUID, CardNumber from CustomerCreditCard_Polling`
+    customerCreditCardPollingSelect: `select CustomerUUID, CardNumber from CustomerCreditCard_Polling`,
+    customerPollingDelete: `delete from CustomerCreditCard_Polling`,
+    creditCardPollingDelete: `delete from CustomerCreditCard_Polling`,
+    customerCreditCardPollingDelete: `delete from CustomerCreditCard_Polling`,
 };
