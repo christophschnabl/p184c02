ec7bcf5c40336950cccd47563796ea3afb93cc78
cschnabl <mail@christoph-schnabl.com>
2019-01-23 19:26:22 +0100

    Import queries module

--- dataset/replication/app.js
+++ dataset/replication/app.js
@@ -1,9 +1,12 @@
 const pool = require('./modules/database.js');
+const queries = require('./modules/queries.js');
 const wait = require('./modules/wait.js');
 
 
 async function replicationLoop() {
     while (true) {
+        console.log('Polling tables...');
+
         await wait(10000);
     }
 }
