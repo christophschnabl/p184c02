f09f042c4a4dedf596a80d2ea347e4a0d21f4fbd
Andreas Horvath <mail@andreas-horvath.at>
2019-01-23 20:18:18 +0100

    Add polling tables to mysql-init.sql

--- dataset/import/mysql-init.sql
+++ dataset/import/mysql-init.sql
@@ -13,6 +13,8 @@ grant all privileges
    to 'frauddetection'@'localhost'
 ;
 
+-- run after create-table
+
 grant select, insert, update, delete
    on Customer
    to 'frauddetection'@'localhost'
@@ -30,6 +32,26 @@ grant select, insert, update, delete
 
 grant select, insert, update, delete
    on Transaction
+   to 'frauddetection'@'localhost'
+;
+
+grant select, insert, update, delete
+   on Customer_Polling
+   to 'frauddetection'@'localhost'
+;
+
+grant select, insert, update, delete
+   on CreditCard_Polling
+   to 'frauddetection'@'localhost'
+;
+
+grant select, insert, update, delete
+   on CustomerCreditCard_Polling
+   to 'frauddetection'@'localhost'
+;
+
+grant select, insert, update, delete
+   on Transaction_Polling
    to 'frauddetection'@'localhost'
 ;
 
