4629dd64a220507f0a2dbfda296dc7f320a0d077
Andreas Horvath <mail@andreas-horvath.at>
2019-01-24 08:41:55 +0100

    Add Action column in polling select statement

--- dataset/replication/modules/queries.js
+++ dataset/replication/modules/queries.js
@@ -1,9 +1,9 @@
 module.exports = {
     customerPollingSelect: `select CustomerUUID, Name, Country, Address, SSN, AccountStatus, Duration, CreditHistory, Purpose, CreditAmount,
         Savings, Employment, InstallmentRate, PersonalStatus, sex, Debtors, Residence, Property, Age,
-        OtherInstallmentPlans, Housing, NumberOfCredits, Job, LiableMaintenance, Telephone, ForeignWorker, Cost from Customer_Polling`,
-    creditCardPollingSelect: `select CardNumber, IssuingNetwork, CVV, ExpirationYear, ExpirationMonth from CreditCard_Polling`,
-    customerCreditCardPollingSelect: `select CustomerUUID, CardNumber from CustomerCreditCard_Polling`,
+        OtherInstallmentPlans, Housing, NumberOfCredits, Job, LiableMaintenance, Telephone, ForeignWorker, Cost, Action from Customer_Polling`,
+    creditCardPollingSelect: `select CardNumber, IssuingNetwork, CVV, ExpirationYear, ExpirationMonth, Action from CreditCard_Polling`,
+    customerCreditCardPollingSelect: `select CustomerUUID, CardNumber, Action from CustomerCreditCard_Polling`,
     customerPollingDelete: `delete from Customer_Polling where CustomerUUID = ?`,
     creditCardPollingDelete: `delete from CreditCard_Polling where CardNumber = ?`,
     customerCreditCardPollingDelete: `delete from CustomerCreditCard_Polling where CustomerUUID = ? and CardNumber = ?`
