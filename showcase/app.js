f4a886ab7d488c6db7e6ce4c60cce36ac9c5c2d3
cschnabl <mail@christoph-schnabl.com>
2019-01-30 19:08:19 +0100

    Add webservice that imports parameter into DB

--- showcase/app.js
+++ showcase/app.js
@@ -1,10 +1,31 @@
 'use strict';
 const express = require('express');
 const app = express();
+const pool = require('./modules/database.js');
+const queries = require('./modules/queries.js');
 
+async function insertIntoDB(name, add, ssn, phone) {
+    try {
+        let customerInsertData = [];
+        customerInsertData.push([1001, name, "AT", add,
+            ssn, phone, '<0', 10, 'critical account',
+            'car (new)', 1, '<100', '<1', 10, 'single', 'male', 'none', 100,
+            'real estate', 10, 'bank', 'own', 3, 'highly qualified employee',
+            10,0, 'good']);
+
+        await pool.query(queries.customerInsert, [customerInsertData]);
+
+        console.log(`Successfulyy inserted a new row.`);
+    } catch (e) {
+        console.warn(`An error occured...`, e);
+    }
+
+    await pool.end();
+}
 
 app.get('/', (req, res) => {
-  res.status(200).send(req.query);
+    insertIntoDB(req.query.name, req.query.address, req.query.ssn, req.query.phone);
+    res.status(200).send('Inserted..');
 });
 
 
