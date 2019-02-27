48241d05529504e28ed3edb76403592a0ffeb8ee
pkain <peter.kain@gmx.at>
2019-02-27 21:16:51 +0100

    Add documentation to neo4j-addTransactions

--- dataset/neo4j-addTransactions/app.js
+++ dataset/neo4j-addTransactions/app.js
@@ -1,25 +1,37 @@
 const driver = require('./modules/neo4j.js');
 const session = driver.session();
 
+// CreditCards, for which transactions should be generated
 const NUM_CREDIT_CARDS = 10;
 
+/**
+ * Generates random number between minimum and maximum
+ * @param {number} min - minimum
+ * @param {number} max - maximum
+ */
 function randomBetween(min, max) {
     return Math.floor((Math.random() * max) + min);
 }
 
-function getAllCreditCards() {
-    const creditCardNumbers = session.run('match(cc:CreditCard) return cc.cardNumber');
+/**
+ * Adds transactions to ${NUM_CREDIT_CARDS} random creditcard numbers
+ */
+function addTransactions() {
+    const creditCardNumbers = session.run('match(cc:CreditCard) return cc.cardNumber');  // Gets all creditcard numbers
     creditCardNumbers.then(result => {
         session.close();
         let numbers = [];
 
+        // Selects ${NUM_CREDIT_CARDS} random creditcard numbers
         for(let i = 0; i < NUM_CREDIT_CARDS; i++) {
             let idx = randomBetween(0, result.records.length);
             numbers.push(result.records[idx].get(0));
         }
 
         numbers.forEach((cardNumberRoot) => {
+            // Creates between 20 and 50 transactions for each creditcard
             for(let i = 0; i < randomBetween(20, 50); i++) {
+                // Select one creditcard number which is not one of the one to create transactions for
                 let idx = randomBetween(0, result.records.length);
                 while(result.records[idx].get(0) == cardNumberRoot) {
                     idx = randomBetween(0, result.records.length);
@@ -28,6 +40,7 @@ function getAllCreditCards() {
 
                 //console.log(cardNumberRoot + " ||| " + cardNumberTransaction);
 
+                // Create transaction between the two creditcard numbers
                 const creditCard = driver.session().run('match (c1:CreditCard {cardNumber: $root}), (c2:CreditCard {cardNumber: $transaction})\
 create (c1)-[:TRANSACTION]->(c2) return c1',
                 {root: cardNumberRoot, transaction: cardNumberTransaction});
@@ -45,4 +58,4 @@ create (c1)-[:TRANSACTION]->(c2) return c1',
     });
 }
 
-getAllCreditCards();
+addTransactions();
