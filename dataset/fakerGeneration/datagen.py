cc6d9d20dd552bb2a176a28a4901137d7478563a
pkain <peter.kain@gmx.at>
2019-02-27 21:07:59 +0100

    Add documentation for fakerGeneration

--- dataset/fakerGeneration/datagen.py
+++ dataset/fakerGeneration/datagen.py
@@ -1,17 +1,18 @@
 #!/bin/env python
 from faker import Faker
-NUMROWS = 1000000
-fake = Faker("en_US")
 
-block = ""
-with open('dataset.csv', 'wb') as f:
-    numrows = 0
+NUMROWS = 100000       # Max Datasets
+fake = Faker("en_US")  # Generate data based on the US dataset
+
+block = ""             # Buffer
+with open('dataset.csv', 'wb') as f:    # = (f = open('dataset.csv', 'wb))
+    numrows = 0                         # counter for NUMROWS
     while numrows < NUMROWS:
-        block += fake.name() + "\n" + str(fake.address()).translate(None, "\n") + "\n"
+        block += fake.name() + ";" + str(fake.address()).translate(None, "\n") + "\n"   # Append Name/Address to buffer
         numrows += 1
-        if len(block) > 64000: #Write Blocks of 64KB
+        if len(block) > 64000: # If buffer size exceeds 64kB - write to file
             f.write(block)
-            block = ""
-    f.write(block)
+            block = ""         # Clear buffer
+    f.write(block)             # Write rest of buffer
 f.close()
         
