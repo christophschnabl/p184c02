91ca76a4721bfec64d6a333e33f3317654afd01f
Andreas Horvath <mail@andreas-horvath.at>
2019-03-12 12:43:04 +0100

    Remove creditcards from transactions

--- dataset/addTransactions/app.js
+++ dataset/addTransactions/app.js
@@ -9,21 +9,7 @@ const MAX_TRANSACTIONS = 15000;
 const MIN_TRANSACTIONS = 10000;
 const NUM_TRANSACTIONS = rBetween(MIN_TRANSACTIONS, MAX_TRANSACTIONS);
 
-
-const creditCardSelect = `select CardNumber from CreditCard`;
 const customerSelect = `select CustomerUUID from Customer`;
-
-
-/**
- * picks random creditcards
- * @param {Array} creditcards
- * @returns {Array.<String>}
- */
-function pickTwoCreditCards(creditcards) {
-    const shuffled = shuffle(creditcards.slice());
-    return shuffled.slice(0, 2);
-}
-
 
 
 /**
@@ -34,30 +20,6 @@ function pickTwoCreditCards(creditcards) {
 function pickTwoCustomers(customers) {
     const shuffled = shuffle(customers.slice());
     return shuffled.slice(0, 2);
-}
-
-
-/**
- * creates an insert query for a creditcard transaction
- * @param {Array.<String>} cards
- */
-function createCreditCardTransactionQuery(cards) {
-    const year = rBetween(2000, 2018);
-    const month = rBetween(1, 12);
-    const day = rBetween(1, 28);
-    const date = `${year}-${month}-${day}`;
-
-    const amount = rBetween(10, 100000);
-
-    const data = [
-        date,
-        amount,
-        cards[0],
-        cards[1]
-    ];
-
-    return pool.query(`insert into Transaction (Date, Amount, CardNumberSender, CardNumberReciever)
-            values (?, ?, ?, ?)`, data);
 }
 
 
@@ -92,13 +54,6 @@ async function addTransactions() {
     try {
         console.log('Adding transactions...');
 
-        const [res, _] =
-            await pool.query(creditCardSelect);
-        let creditcards = [];
-        for (let i = 0; i < res.length; i++) {
-            creditcards.push(res[i].CardNumber);
-        }
-
         const [res2, _2] =
             await pool.query(customerSelect);
         let customers = [];
@@ -110,9 +65,7 @@ async function addTransactions() {
             new Array(NUM_TRANSACTIONS)
                 .fill(0)
                 .map(i =>
-                    rBetween(0, 1) === 0 ?
-                        createCreditCardTransactionQuery(pickTwoCreditCards(creditcards)) :
-                        createCustomerTransactionQuery(pickTwoCustomers(customers))
+                    createCustomerTransactionQuery(pickTwoCustomers(customers))
                 )
         );
 
