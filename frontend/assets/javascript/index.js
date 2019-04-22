e7730c45ee6b9ef3ef763bf56e5fd3d14506f903
cschnabl <mail@christoph-schnabl.com>
2019-04-22 13:57:16 +0200

    Change login credentials - now gcloud docker container

--- frontend/assets/javascript/index.js
+++ frontend/assets/javascript/index.js
@@ -1,9 +1,9 @@
 import Navigo from './navigo';
 
 
-const neo4jURI = 'bolt://127.0.0.1:7687';
-const neo4jUSER = 'neo4j';
-const neo4jPASS = 'andreas';
+const neo4jURI = 'bolt://34.65.38.108:7687';
+const neo4jUSER = 'neo4j2';
+const neo4jPASS = 'neo4j';
 
 /**
  * draw function
